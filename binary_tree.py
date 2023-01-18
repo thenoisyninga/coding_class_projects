2c16ab57dcd13d52a3a1d32117a8177e8cbeec81
thenoisyninga <C41f0N@protonmail.com>
2023-01-18 16:28:56 +0500

    did searching on binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -1,49 +1,34 @@
-
-
 class Node:
     def __init__(self):
         self.left_pointer = None
         self.right_pointer = None
         self.value = None
 
-
-root = Node()
-
-
 tree = []
 
 for i in range(10):
     tree.append(Node())
 
-
-
-
-
-
-
-
 root_pointer = None
 next_pointer = 0
 
 
-
-
-def insert(value):
+def insert(insert_value):
     global tree
     global root_pointer
     global next_pointer
 
     if root_pointer is None:
         root_pointer = 0
-        tree[next_pointer].value = value
+        tree[next_pointer].value = insert_value
         next_pointer += 1
     else:
-        tree[next_pointer].value = value
+        tree[next_pointer].value = insert_value
         placed = False
         current_pointer = root_pointer
 
         while not placed:
-            if value > tree[current_pointer].value:
+            if insert_value > tree[current_pointer].value:
                 if tree[current_pointer].right_pointer is not None:
                     current_pointer = tree[current_pointer].right_pointer
                 else:
@@ -64,12 +49,34 @@ def print_tree():
         print(f"{node.left_pointer} {node.value} {node.right_pointer}")
     print("")
 
-print_tree()
+def search(search_value):
+    global tree
+    global root_pointer
+
+    current_pointer = root_pointer
+    found = False
+
+    while not found:
+        if current_pointer is None:
+            break
+        else:
+            if search_value == tree[current_pointer].value:
+                found = True
+            elif search_value > tree[current_pointer].value:
+                current_pointer = tree[current_pointer].right_pointer
+            else:
+                current_pointer = tree[current_pointer].left_pointer
+
+    return found
+
+
 insert(10)
+insert(12)
 insert(11)
-insert(9)
-insert(8)
-insert(7)
-insert(0)
+insert(4)
+insert(1)
+insert(6)
+insert(78)
 print_tree()
 
+print(search(0))
--- test.py
+++ test.py
@@ -1,20 +1,17 @@
-marksObt = float(input("Enter your Marks: "))
-marksTot = int(input("Total Marks: "))
+
+class Node:
+    def __init__(self):
+        self.left_pointer = None
+        self.right_pointer = None
+        self.value = None
 
 
-percentage = (marksObt/marksTot) * 100
 
-if percentage >= 90:
-    print("Your grade is A")
-elif percentage >= 80:
-    print("Your grade is B.")
-elif percentage >= 70:
-    print("Your grade is C")
-elif percentage >= 60:
-    print("Your grade is D")
-elif percentage >= 50:
-    print("Your grade is E")
-else:
-    print("U")
+tree = []
 
+for i in range(10):
+    tree.append(Node())
+
+root_pointer = None
+next_pointer = 0
 
