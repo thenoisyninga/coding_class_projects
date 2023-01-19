321ee3bb65f3024aeb5c03631b9faddfe51cf70d
thenoisyninga <C41f0N@protonmail.com>
2023-01-19 15:36:52 +0500

    started linked lists

---
+++ linked_list.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data, pointer):
+        self.data = data
+        self.pointer = pointer
+
+linked_list = []
+
+linked_list.append(Node(20, 4))
+linked_list.append(Node(5, 3))
+linked_list.append(Node(0, 3))
+linked_list.append(Node(9, 6))
+linked_list.append(Node(25, 5))
+linked_list.append(Node(78, None))
+linked_list.append(Node(13, 0))
+
+head = 2
+next_pointer = 7
+def show_linked_list():
+    i = 0
+    print(f"head --> {head}")
+    print("Index Data  Pointer")
+    for node in linked_list:
+        print(f"[{i}]   {node.data}" + ("  " if len(str(node.data)) == 1 else " ") + f"-> {node.pointer}")
+        i += 1
+
+show_linked_list()
+
--- test.py
+++ test.py
@@ -1,65 +1,9 @@
-
-class Node:
-    def __init__(self):
-        self.left_pointer = None
-        self.right_pointer = None
-        self.value = None
-
-
-
-tree = []
-
-for i in range(10):
-    tree.append(Node())
-
-root_pointer = None
-next_pointer = 0
-
-def insert(insert_value):
-    global tree
-    global root_pointer
-    global next_pointer
-
-    if root_pointer is None:
-        root_pointer = 0
-        tree[next_pointer].value = insert_value
-        next_pointer  += 1
-    else:
-        tree[next_pointer].value = insert_value
-        placed = False
-        current_pointer = root_pointer
-
-        while not placed:
-            if insert_value > tree[current_pointer].value:
-                if tree[current_pointer].right_pointer is not None:
-                    current_pointer = tree[current_pointer].right_pointer
-                else:
-                    tree[current_pointer].right_pointer = next_pointer
-                    next_pointer += 1
-                    placed = True
-            else:
-                if tree[current_pointer].left_pointer is not None:
-                    current_pointer = tree[current_pointer].left_pointer
-                else:
-                    tree[current_pointer].left_pointer = next_pointer
-                    next_pointer += 1
-                    placed = True
-
-
-def print_tree():
-    global tree
-    for node in tree:
-        print(f"{node.left_pointer} {node.value} {node.right_pointer}")
-    print("")
-
-
-
-print_tree()
-insert(9)
-insert(8)
-insert(7)
-insert(6)
-insert(11)
-insert(14)
-
-print_tree()
+# def print_data():
+#     print("Printing Data..")
+#     current_pointer = head
+#     while current_pointer is not None:
+#         print(linked_list[current_pointer].data)
+#         current_pointer = linked_list[current_pointer].pointer
+# 
+# print("")
+# print_data()
